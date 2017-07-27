50ad085125c0ad7a06b331839e05cec3af9d2964
Letitbe133 <lionel.lft@me.com>
2017-07-27 18:37:55 +0200

    traitement bugs formulaire de contact

--- js/main.js
+++ js/main.js
@@ -35,47 +35,12 @@ $(function(){
     $('#modal').modal('close');
   })
 
-  // // Initialisation toast
-  // $('button[type=submit]').on("click", function(){
-  //   setTimeout(function() {
-  //     var feedback = $('.feedback').text();
-  //     if(feedback.length > 0){
-  //       Materialize.toast(feedback, 7000);
-  //     }
-  //   }, 1000);
-  //   })
-
-  const MutationObserver = window.MutationObserver ||
-  window.WebKitMutationObserver ||
-  window.MozMutationObserver
-
-
-  var observer = new MutationObserver((changes) => {
-    console.log(changes);
-    changes.forEach((change) => {
-      console.log('Mutation', change);
-      if(change.type === 'characterData') {
-        console.info('text content', change.target.nodeValue)
-      }
-    });
-  });
-
-  var target = document.querySelector('#feedback');
-
-  var config = {
-    attributes: true,
-    childList: true,
-    characterData: true,
-    subtree: true
-  };
-
-  observer.observe(target, config);
-
-
+// Traitement du formulaire en ajax
 
   // D'abord on sélectionne les éléments HTML pour pouvoir travailler
   // avec ensuite
   const form = document.querySelector('form')
+  const submit = document.querySelector('#submit')
   const nom = document.querySelector('#name')
   const email = document.querySelector('#email')
   const objet = document.querySelector('#subject')
@@ -83,7 +48,7 @@ $(function(){
 
   // On créé une fonction handleForm qui va gérer le formulaire
   // quand celui-ci sera submit par le visiteur
-  // Elle reçoit l'événement (e) pour pouvoir stopper 
+  // Elle reçoit l'événement (e) pour pouvoir stopper
   // l'envoi classique du formulaire
   const handleForm = (e) => {
     // On stoppe l'action classique de l'événement submit
@@ -92,6 +57,7 @@ $(function(){
     // On créé un objet FormData qui est fait pour contenir les données
     // du formulaire afin de les envoyer via AJAX au serveur
     const data = new FormData()
+    data.append('submit', submit.value)
     data.append('nom', nom.value)
     data.append('email', email.value)
     data.append('objet', objet.value)
@@ -105,7 +71,7 @@ $(function(){
     // et que la réponse sera revenue et chargée (d'où le onload)
     ajaxRequest.onload = () => {
       // On récupère le résultat affiché par la page contact.php
-      // Il faut donc que contact.php fasse un echo du $feedback ! 
+      // Il faut donc que contact.php fasse un echo du $feedback !
       // Sinon la requete AJAX ne ramènera rien ..
       const resultat = ajaxRequest.responseText;
 
@@ -116,7 +82,7 @@ $(function(){
         // - "Super " . $nom . "! On revient vers vous rapido ;)"
         // - "Ooops ! Il doit manquer quelque chose... "
         // Si le $feedback est vide, on ne rentrera pas dans ce if
-        Materialize.toast(resultat, 4000) 
+        Materialize.toast(resultat, 5000)
       }
     }
 
@@ -124,6 +90,7 @@ $(function(){
     // donc on l'envoi vers le serveur
     ajaxRequest.send(data)
 
+    form.reset()
   }
 
   // On oublie pas de dire au formulaire que quand il sera
